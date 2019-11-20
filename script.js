bd4ce91c3f36eff1c61742408fb586259550b232
AJ Meyer <ashley.meyer@gmail.com>
2019-11-20 14:50:33 -0600

    added alerts

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 // Write your JavaScript code here!
 
-let button = document.getElementById("formSubmit");
-// add event handler for when button clicked
+window.addEventListener("load", function() {
+
+let form = document.getElementById("launchForm");
+let button = document.getElementById("submit");
+
 button.addEventListener("click", function() {
-   let pilot = document.getElementById("pilotName");
-   let copilot = document.getElementById("copilotName");
-   let fuel = document.getElementById("fuelLevel");
-   let mass = document.getElementById("cargoMass");
-   if (pilot.value === "" || copilot.value === "" || fuel.value === "" || mass.value === "") {
+   let pilotName = document.getElementById("pilot");
+   let copilotName = document.getElementById("copilot");
+   let fuelLevel = document.getElementById("fuel");
+   let cargoMass = document.getElementById("mass");
+   console.log(pilotName);
+
+   if (pilotName.value == "" || copilotName.value == "" || fuelLevel.value == "" || cargoMass.value == "") {
       alert("All fields are required!");
       event.preventDefault();
-   }
+      }
+
+      if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+         alert("Fuel Level and Cargo Mass must be numbers.");
+         event.preventDefault();
+       }
+   });
 });
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
@@ -25,17 +37,4 @@ button.addEventListener("click", function() {
 <img src="${}">
 */
 
-// from the BOOK: 
-// window.addEventListener("load", function() {
-//    let form = document.querySelector("form");
-//    form.addEventListener("submit", function(event) {
-//       let usernameInput = document.querySelector("input[name=username]");
-//       let teamName = document.querySelector("input[name=team]");
-//       if (usernameInput.value === "" || teamName.value === "") {
-//          alert("All fields are required!");
-//          // stop the form submission
-//          event.preventDefault();
-//       }
-//    });
-// });
 
